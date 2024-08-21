979150a46b37be753c6c6e4a4b754f11b65763b3
debasish-itobuz <debasish.das@itobuz.com>
2024-08-21 15:27:15 +0530

    updated

--- backend/src/multerConfig.ts
+++ backend/src/multerConfig.ts
@@ -21,39 +21,3 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 export default upload;
-
-
-
-// const uploadVideo = async (req: Request, res: Response) => {
-//   // console.log("...",req.file)
-//   try {
-//     if (req?.file?.path) {
-//       const userId = req.query.id;
-//       const videos = req.file;
-//       // console.log("obj", videos)
-
-//       //const allvideos = await userModel.findById(userId, 'videos');
-
-      
-//       if (!userId)
-//         return res.status(400).send({ message: "User ID is required" });
-
-//       const data = await userModel.findByIdAndUpdate(userId, {
-//          $push: { videos: {title: videos.filename, url: videos.path}}
-//       });
-
-//       if (!data) return res.status(400).send({ message: "User not found" });
-
-//       return res.status(200).send({
-//         data: { ...data, videos },
-//         message: "video uploaded successfully",
-//       });
-//     } else {
-//       return res.status(400).send({ message: "Video path not recieved" });
-//     }
-//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   } catch (e: any | ZodError) {
-//     console.error("Error in uploadVideo:", e);
-//     return catchBlock(e, res, "Video not uploaded");
-//   }
-// };
